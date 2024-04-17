e984750ddc6546318e540d5b90105b71061d0e4a
Aleks <aleksanderlapinski@wp.pl>
2024-04-17 21:04:41 +0200

    Deconstruct main method and reformat the logic

--- color_tracker.py
+++ color_tracker.py
@@ -59,21 +59,56 @@ class ColorTracker:
         self._video = cv2.VideoCapture(video_path)
         if not self._video.isOpened():
             raise FileNotFoundError('Unable to open video file')
-        self._tolerances_dict = {
-            HUE_INDEX: hue_tolerance,
-            SATURATION_INDEX: saturation_tolerance,
-            VALUE_INDEX: value_tolerance
-        }
         self._tracked_color: None | tuple[int, int, int] = None
         self._frame: None | np.ndarray = None
         self._processed_frame: None | np.ndarray = None
         self._processing_type: ProcessingType = ProcessingType.RAW
         self._erosion_depth: int = 0
         self._dilation_depth: int = 0
+
+        self._tolerances_dict = {
+            HUE_INDEX: hue_tolerance,
+            SATURATION_INDEX: saturation_tolerance,
+            VALUE_INDEX: value_tolerance
+        }
         self._preprocessing_depth_handler = {
             PreprocessingType.ERODE: self._increment_erode_depth,
             PreprocessingType.DILATE: self._increment_dilate_depth
         }
+        self._processing_orchestrator = {
+            ProcessingType.RAW: self._get_current_frame,
+            ProcessingType.HUE: self._prepare_hue_layer,
+            ProcessingType.SATURATION: self._prepare_saturation_layer,
+            ProcessingType.VALUE: self._prepare_value_layer,
+            ProcessingType.MASK: self._prepare_mask_layer,
+            ProcessingType.TRACKER: self._prepare_tracking_layer
+        }
+
+    def get_processed_frame(self) -> np.ndarray:
+        """
+        Method returning altered current frame.
+        :return: a copy of processed frame
+        """
+        temp = self._processed_frame.copy()
+        return temp
+
+    def set_processing_type(self, processing_type: ProcessingType) -> None:
+        """
+        Setter method fot _processing_type property.
+        :param processing_type:
+        :return: None
+        """
+        self._processing_type = processing_type
+
+    def set_reference_color(self, x: int, y: int) -> None:
+        """
+        Setter method fot _tracked_color property.
+        :param x: X coordinate of cursor
+        :param y: Y coordinate of cursor
+        :return: None
+        """
+        hsv_frame: np.ndarray = cv2.cvtColor(self._frame, cv2.COLOR_RGB2HSV)
+        self._tracked_color = hsv_frame[y, x, :]
 
     def update_ed_depths(self, preprocess: PreprocessingType) -> None:
         """
@@ -82,6 +117,17 @@ class ColorTracker:
         :return: None
         """
         self._preprocessing_depth_handler[preprocess]()
+
+    def update_frame(self) -> bool:
+        """
+        Method responsible for continuous video updating.
+        :return: bool: True if video was read successfully, False otherwise.
+        """
+        read_successful, self._frame = self._video.read()
+        if read_successful:
+            self._preprocess_frame()
+            self._process_frame()
+        return read_successful
 
     def _increment_erode_depth(self) -> None:
         """
@@ -119,34 +165,17 @@ class ColorTracker:
         kernel = np.ones((5, 5), np.uint8)
         return cv2.dilate(img, kernel, iterations=self._dilation_depth)
 
-    def set_processing_type(self, processing_type: ProcessingType) -> None:
+    def _validate_color(self) -> None:
         """
-        Setter method fot _processing_type property.
-        :param processing_type:
+        Method responsible for perform None-check on _tracked_color property.
+        Throws ValueError when _tracked_color is set to None.
         :return: None
         """
-        self._processing_type = processing_type
+        if self._tracked_color is None:
+            raise ValueError(TRACKING_COLOR_ERROR)
 
-    def set_reference_color(self, x: int, y: int) -> None:
-        """
-        Setter method fot _tracked_color property.
-        :param x: X coordinate of cursor
-        :param y: Y coordinate of cursor
-        :return: None
-        """
-        hsv_frame: np.ndarray = cv2.cvtColor(self._frame, cv2.COLOR_RGB2HSV)
-        self._tracked_color = hsv_frame[y, x, :]
-
-    def update_frame(self) -> bool:
-        """
-        Method responsible for continuous video updating.
-        :return: bool: True if video was read successfully, False otherwise.
-        """
-        read_successful, self._frame = self._video.read()
-        if read_successful:
-            self._preprocess_frame()
-            self._process_frame()
-        return read_successful
+    def _get_current_frame(self) -> np.ndarray:
+        return self._frame
 
     def _get_layer(self, layer_index: int) -> np.ndarray:
         """
@@ -156,57 +185,6 @@ class ColorTracker:
         """
         return cv2.cvtColor(self._frame, cv2.COLOR_RGB2HSV)[:, :, layer_index]
 
-    def _process_frame(self) -> None:
-        """
-        Method responsible for altering current frame based on the selected processing type.
-        :return: None
-        """
-        if self._processing_type is ProcessingType.RAW:
-            self._processed_frame = self._frame
-            return
-
-        hue_layer = self._get_layer(HUE_INDEX)
-        saturation_layer = self._get_layer(SATURATION_INDEX)
-        value_layer = self._get_layer(VALUE_INDEX)
-
-        merged_mask, frame_with_color_tracking = None, None
-        if self._processing_type in self._COLOR_RELIANT_PROCESSES:
-            mask_hue = self._get_mask(hue_layer, HUE_INDEX)
-            mask_saturation = self._get_mask(saturation_layer, SATURATION_INDEX)
-            mask_value = self._get_mask(value_layer, VALUE_INDEX)
-            merged_mask = (mask_value & mask_saturation & mask_hue).astype(np.uint8)
-            frame_with_color_tracking = self._get_tracked_color_frame(merged_mask)
-
-        if self._processing_type == ProcessingType.HUE:
-            self._processed_frame = hue_layer
-            return
-
-        if self._processing_type == ProcessingType.SATURATION:
-            self._processed_frame = saturation_layer
-            return
-
-        if self._processing_type == ProcessingType.VALUE:
-            self._processed_frame = value_layer
-            return
-
-        if self._processing_type == ProcessingType.MASK:
-            self._processed_frame = merged_mask
-            return
-
-        if self._processing_type == ProcessingType.TRACKER:
-            self._processed_frame = frame_with_color_tracking
-            return
-
-    def _preprocess_frame(self) -> None:
-        """
-        Method responsible for preprocessing the frame - apply erosion and dilatation accordingly to
-        private depth variables incremented by user.
-        :return:
-        """
-        eroded = self._erode(self._frame)
-        eroded_and_dilated = self._dilate(eroded)
-        self._frame = eroded_and_dilated
-
     def _get_mask(self, mask: np.ndarray, index: int) -> np.ndarray:
         """
         Method responsible for preparing appropriate value detection in specified layer.
@@ -214,8 +192,6 @@ class ColorTracker:
         :param index: index of the layer (Hue:0, Saturation:1, Value:2)
         :return: black/white result mask based on the tolerance and selected color
         """
-        if self._tracked_color is None:
-            raise ValueError(TRACKING_COLOR_ERROR)
         related_tolerance = self._tolerances_dict[index]
         left_inner = np.where(mask <= self._tracked_color[index] + related_tolerance,
                               mask,
@@ -228,8 +204,9 @@ class ColorTracker:
     def _get_tracked_color_frame(self, mask: np.ndarray) -> np.ndarray:
         """
         Method responsible for enclosing detected shape in rectangle based on merged HSV masks.
-        Bounding box consists of 4 coordinates (x1, y1, x2, y2) that are marking corners of the bounding box.
-        None-check performed to handle situations when the shape is absent on the screen.
+        Bounding box consists of 4 coordinates (x1, y1, x2, y2) that are marking corners of the
+        bounding box. None-check performed to handle situations -
+        - when the shape is absent on the screen.
         :param mask: merged H, S and V masks to one mask
         :return: frame with framed object of a certain color
         """
@@ -243,13 +220,63 @@ class ColorTracker:
                                 (x1, y1), (x2, y2), WHITE_VALUE, 2)
         return drawing
 
-    def get_processed_frame(self) -> np.ndarray:
+    def _prepare_hue_layer(self) -> np.ndarray:
         """
-        Method returning altered current frame.
-        :return: a copy of processed frame
+        Method responsible for retrieving hue layer of HSV frame
+        :return: np.ndarray: Hue layer HSV frame
         """
-        temp = self._processed_frame.copy()
-        return temp
+        return self._get_layer(HUE_INDEX)
+
+    def _prepare_saturation_layer(self) -> np.ndarray:
+        """
+        Method responsible for retrieving saturation layer of HSV frame
+        :return: np.ndarray: Saturation layer of HSV frame
+        """
+        return self._get_layer(SATURATION_INDEX)
+
+    def _prepare_value_layer(self) -> np.ndarray:
+        """
+        Method responsible for retrieving value layer of HSV frame
+        :return: np.ndarray: Value layer of HSV frame
+        """
+        return self._get_layer(VALUE_INDEX)
+
+    def _prepare_mask_layer(self) -> np.ndarray:
+        """
+        Method responsible for creating color specific mask layer, consisting of all hue,
+        saturation and value layers.
+        :return: np.ndarray: Merged color-specific mask
+        """
+        self._validate_color()
+        mask_hue = self._get_mask(self._prepare_hue_layer(), HUE_INDEX)
+        mask_saturation = self._get_mask(self._prepare_saturation_layer(), SATURATION_INDEX)
+        mask_value = self._get_mask(self._prepare_value_layer(), VALUE_INDEX)
+        return (mask_value & mask_saturation & mask_hue).astype(np.uint8)
+
+    def _prepare_tracking_layer(self) -> np.ndarray:
+        """
+        Method responsible for creating color specific mask layer, consisting of all hue,
+        saturation and value layers.
+        :return: np.ndarray: Merged color-specific mask
+        """
+        return self._get_tracked_color_frame(self._prepare_mask_layer())
+
+    def _preprocess_frame(self) -> None:
+        """
+        Method responsible for preprocessing the frame - apply erosion and dilatation accordingly to
+        private depth variables incremented by user.
+        :return:
+        """
+        eroded = self._erode(self._frame)
+        eroded_and_dilated = self._dilate(eroded)
+        self._frame = eroded_and_dilated
+
+    def _process_frame(self) -> None:
+        """
+        Method responsible for altering current frame based on the selected processing type.
+        :return: None
+        """
+        self._processed_frame = self._processing_orchestrator[self._processing_type]()
 
 
 class Display:
